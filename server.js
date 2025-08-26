178e1e9b6c7f273b9771ce3ea7ff7cc86d8a86c7
Sankar <149525059+mr-sankar@users.noreply.github.com>
2025-08-26 15:48:10 +0530

    Update server.js
    final changes

--- server.js
+++ server.js
@@ -85,7 +85,11 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:8080",
+    origin: [
+      "http://localhost:5173", // local vite
+      "http://localhost:8080", // if you used 8080 before
+      "https://sindhuur-frontend-9gsqlcz65-kella-sankars-projects.vercel.app" // deployed frontend
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   },
@@ -2562,3 +2566,4 @@ const startServer = async () => {
 };
 
 startServer();
+
